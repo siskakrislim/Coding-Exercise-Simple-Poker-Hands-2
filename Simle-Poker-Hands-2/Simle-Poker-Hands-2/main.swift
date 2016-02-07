6440dad04f71cd19f25237b843050c493e5cb61b
Bartosz Oczujda <bart@bartoszs-mac.local>
2016-02-07 18:56:34 +0100

    Initial xcode setup

---
+++ Simle-Poker-Hands-2/Simle-Poker-Hands-2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Simle-Poker-Hands-2
+//
+//  Created by Bartosz Oczujda on 07/02/16.
+//  Copyright © 2016 Pixelrockit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
